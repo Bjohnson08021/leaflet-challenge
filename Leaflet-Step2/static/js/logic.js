6ff7a9ab74e7ec5a19fa1a364e3aebb35cea5b16
Bjohnson08021 <84213057+Bjohnson08021@users.noreply.github.com>
2021-10-01 19:55:52 -0400

    working on layer

--- Leaflet-Step2/static/js/logic.js
+++ Leaflet-Step2/static/js/logic.js
@@ -1,11 +1,39 @@
 // Store our API endpoint as queryUrl.
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
+var tectonicData = L.layerGroup();
+var earthquakeData = L.layerGroup();
+
+ // Create the base layers.
+ var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+// adding layers
+var grayscale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
 });
+
+var world = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var nightView = L.tileLayer('https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+minZoom: 1,
+maxZoom: 8,
+format: 'jpg',
+time: '',
+tilematrixset: 'GoogleMapsCompatible_Level'
+});
+
+//---------------------------------------------------
+
 //console.log(data.features)
 
 function createFeatures(earthquakeData) {
@@ -15,6 +43,17 @@ function createFeatures(earthquakeData) {
   function onEachFeature(feature, layer) {
     layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date and Time: ${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p>`);
   }
+
+  
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  // Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
   //adjusting marker size setting colors
     function radius(magnitude) {
         return magnitude * 25000;
@@ -59,10 +98,6 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes) {
 
-  // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
 
   
   // Create a baseMaps object.
@@ -86,12 +121,6 @@ function createMap(earthquakes) {
   });
   
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
 
 
   // Set up the legend.
@@ -117,9 +146,6 @@ return div;
   legend.addTo(myMap);
 
 }
-
-
-
-  
+})
 
 }
