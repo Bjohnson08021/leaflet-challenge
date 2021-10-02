1f42235b2d96081b8786e011c53b1d4037ab8468
Bjohnson08021 <84213057+Bjohnson08021@users.noreply.github.com>
2021-10-02 09:41:45 -0400

    modified layer pulled tactonic plate data

--- Leaflet-Step2/static/js/logic.js
+++ Leaflet-Step2/static/js/logic.js
@@ -32,98 +32,88 @@ time: '',
 tilematrixset: 'GoogleMapsCompatible_Level'
 });
 
-//---------------------------------------------------
-
-//console.log(data.features)
-
-function createFeatures(earthquakeData) {
-
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date and Time: ${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p>`);
-  }
-
+// Create a baseMaps object.
+var baseMaps = {
+  "Street Map": street,
+  "Grayscale": grayscale,
+  "World": world,
+  "Night View": nightView
   
-  // Create a layer control.
+};
+
+// Create an overlay object to hold our overlay.
+var overlayMaps = {
+  Earthquakes: earthquakeData,
+  TectonicData: tectonicData
+};
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+var myMap = L.map("map", {
+  center: [
+    37.09, -95.71
+  ],
+  zoom: 5,
+  layers: [street, earthquakeData]
+});
+
+// Create a layer control.
   // Pass it our baseMaps and overlayMaps.
   // Add the layer control to the map.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 
-  // Perform a GET request to the query URL/
+
+
+// Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
-  //adjusting marker size setting colors
-    function radius(magnitude) {
-        return magnitude * 25000;
+   //adjusting marker size setting colors
+   function radius(magnitude) {
+    return magnitude * 25000;
+}
+function markerColor(magnitude){
+    if (magnitude >= 5) {
+        return "red"
     }
-    function markerColor(magnitude){
-        if (magnitude >= 5) {
-            return "red"
-        }
-        else if (magnitude >= 4) {
-            return "orange"
-        }
-        else if (magnitude >= 3) {
-            return "yellow"
-        }
-        else if (magnitude >=2){
-            return "#93eaa8"
-        }
-        else if (magnitude >=1){
-            return "#b1f2ff"
-        }
-        else {
-            return "#949494"
-        }
+    else if (magnitude >= 4) {
+        return "orange"
     }
-
-
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    else if (magnitude >= 3) {
+        return "yellow"
+    }
+    else if (magnitude >=2){
+        return "#93eaa8"
+    }
+    else if (magnitude >=1){
+        return "#b1f2ff"
+    }
+    else {
+        return "#949494"
+    };
+};
+// Create a GeoJSON layer that contains the features array on the earthquakeData object.
   // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    pointToLayer: function(earthquakeData, latlng){
+  L.geoJSON(data, {
+    pointToLayer: function(earthquakeDatas, latlng){
         return L.circle(latlng, {
-            radius: radius(earthquakeData.properties.mag),
-            color: markerColor(earthquakeData.properties.mag)
+            radius: radius(earthquakeDatas.properties.mag),
+            color: markerColor(earthquakeDatas.properties.mag)
         });
     },
-    onEachFeature: onEachFeature
-  });
+    onEachFeature: function(feature, layer){
+      layer.bindPopup(`<h3>Location: ${feature.properties.place}</h3><hr><p>Date and Time: ${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p>`);
+    }
+    }).addTo(earthquakeData);
+    earthquakeData.addTo(myMap);
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-
-
-function createMap(earthquakes) {
-
-
-  
-  // Create a baseMaps object.
-  var baseMaps = {
-    "Street Map": street,
-    
-  };
-
-  // Create an overlay object to hold our overlay.
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
-  });
-  
-
-
-
-  // Set up the legend.
+    d3.json(tectonicUrl).then(function (tecData) {
+      L.geoJSON(tecData,{
+        color: "orange", 
+      }).addTo(tectonicData);
+      tectonicData.addTo(myMap);   
+    });
+//console.log(tectonicUrl)
+     // Set up the legend.
   var legend = L.control({ position: "bottomright" });
   legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
@@ -145,7 +135,28 @@ return div;
   // Adding the legend to the map
   legend.addTo(myMap);
 
-}
-})
+  console.log("tectonic data", tectonicData)
+  console.log("earthquake", earthquakeData)
+  
+  
+  
+});
+//console.log(data.features)
 
-}
+
+ 
+
+  
+
+
+
+
+ 
+  
+  
+
+  
+
+
+
+ 
